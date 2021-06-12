ed2ddd2b407f34e6dd837f5b727d7d7020ce6f18
ahmetcskn <ahmet.coskun.io@hotmail.com>
2021-06-12 16:30:11 +0300

    asd.

---
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,36 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Product product = new Product(1,"Monster Laptop","16 GB Ram",5000, 3, "Siyah" );
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		//product.setId(1);
+		//product.setPrice(5000);
+		//product.setDescription("16 GB Ram");
+		//product.setName("Monster Laptop");
+		//product.setStockAmount(3);
+		//product.setRenk("Siyah");
+		//System.out.println(product.getKod());
+		
+		
+	
+	
+	}
+
+}
